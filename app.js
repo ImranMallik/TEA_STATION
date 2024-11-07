310560abcb73b0725fba8b0b60fb2112c772edab
ImranMallik <imran80mallik@gmail.com>
2024-11-08 00:43:33 +0530

    Done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
+// Set Date
+
 
 navBtn.addEventListener("click", () => {
   navbar.classList.add("showNav");
@@ -9,3 +11,5 @@ navBtn.addEventListener("click", () => {
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
 });
+
+document.getElementById("year").textContent = new Date().getFullYear();
